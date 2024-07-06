0f4f76e96b70dd00eba454bb4ea21b01a5a525a1
PS Developer's <panneer.developer@gmail.com>
2024-07-06 19:06:28 +0530

    initial commit

--- lib/account_picker_plus.dart
+++ lib/account_picker_plus.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/services.dart';
 
+/// AccountPickerPlus
 class AccountPickerPlus {
   static const MethodChannel _channel = MethodChannel('account_picker_plus');
 
+  /// Get Phone number
   static Future<String?> phoneHint() async {
     final String? phone = await _channel.invokeMethod('requestPhoneHint');
     return phone ?? "";
   }
 
+  /// Get Email data from list
   static Future<List<Object?>> emailHint() async {
     final List<dynamic> email = await _channel.invokeMethod('requestEmailHint');
     return email;
